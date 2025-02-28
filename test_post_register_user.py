bcfb8705456dd9e7cb7be46f70e7c4130a975fb0
AssafHIT <asafyehezkel237@gmail.com>
2025-02-28 08:50:20 +0200

    missing_params

--- test_post_register_user.py
+++ test_post_register_user.py
@@ -11,5 +11,30 @@ def test_register_user_happy_flow():
     )
     assert response.status_code == 200
     assert response.ok == True 
-    
     assert 'token' and 'id' in response.json().keys()
+    
+def test_register_user_missing_password():
+    register_user_data = {
+                            "email": "eve.holt@reqres.in"
+                         }
+    response = requests.post(
+        url = "https://reqres.in/api/register",
+        json = register_user_data
+    )
+    assert response.status_code == 400
+    assert response.ok == False 
+    
+    assert response.json()["error"] == "Missing password"
+    
+def test_register_user_missing_email():
+    register_user_data = {
+                            "password": "pistol"
+                         }
+    response = requests.post(
+        url = "https://reqres.in/api/register",
+        json = register_user_data
+    )
+    assert response.status_code == 400
+    assert response.ok == False 
+    
+    assert response.json()["error"] == "Missing email or username"
