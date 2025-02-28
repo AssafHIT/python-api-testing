269e32c1eece834fb0bca10f8988e4a266a34646
AssafHIT <asafyehezkel237@gmail.com>
2025-02-28 09:46:51 +0200

    delete_case

---
+++ test_delete_user.py
@@ -0,0 +1,7 @@
+import requests
+import pytest
+
+@pytest.mark.parametrize("id", [2]) # Brackets for Passing id as Int
+def test_delete_user_by_id(id):
+    response = requests.delete(f"http://reqres.in//api/users/{id}")
+    assert response.status_code == 204 # No Content
